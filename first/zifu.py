74b7b53c3c18fd3f8a3adda6da3a6fb0bb52571d
grandylee <45406700@qq.com>
2019-02-20 17:25:34 +0800

    upload files

--- first/zifu.py
+++ first/zifu.py
@@ -1,14 +1,22 @@
-import random
+# import random
+#
+# a = 'eqwsdqweqwaqaq'
+# b = ''
+# x = len(a)
+# print(x)
+#
+# for i in range(x-1):
+#     if a[i] == 'w' or a[i] == 'q':
+#         pass
+#     else:
+#         b = b+a[i]
+# print(a)
+# print(b)
 
-a = 'eqwsdqweqwaqaq'
-b = ''
-x = len(a)
-print(x)
 
-for i in range(x-1):
-    if a[i] == 'w' or a[i] == 'q':
-        pass
-    else:
-        b = b+a[i]
-print(a)
-print(b)
+#!/usr/bin/env python
+
+
+f = open('放管服.postman_test_run20190220.json', 'rb')
+test_json = f.read(-1)
+print(test_json)
