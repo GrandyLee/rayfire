24df6a9b62ea0f047bed85a6dabc8ea64cf6db95
grandylee <45406700@qq.com>
2019-02-25 14:30:09 +0800

    upload files

--- first/hanoi.py
+++ first/hanoi.py
@@ -1,14 +1,17 @@
+i = 0
+
+
 def hanoi(n, x, y, z):
+    global i
     if n == 1:
-        print(x, '-->', z)
+        i += 1
+        print("移动第", i, "次", x, '-->', z)
     else:
         hanoi(n-1, x, z, y)  # 将前n-1的盘子从x移动到y上3
-
-        print(x, '-->', z)   # 将最底下的盘子从x移动到z上
+        hanoi(1, x, '-->', z)   # 将最底下的一个盘子从x移动到z上
         hanoi(n-1, y, x, z)  # 将y上的n-1个盘子移动到z上
 
 
 n = int(input('请输入汉诺塔的层数：'))
 hanoi(n, 'x', 'y', 'z')
-count = 0
-
+print("总共需要移动至少"+str(i)+"次！")
